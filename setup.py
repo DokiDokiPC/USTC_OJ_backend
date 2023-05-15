706ba6799ea6bb3649d877519d41959fea9e3bb3
Tanix <txlu@mail.ustc.edu.cn>
2023-05-15 17:38:26 +0800

    upgrade

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ setup(
     packages=['backend'],
     install_requires=[
         'argon2-cffi==21.3.0',
-        'Flask==2.2.2',
-        'Flask_Admin==1.6.0',
+        'Flask==2.3.2',
+        'Flask_Admin==1.6.1',
         'Flask_Cors==3.0.10',
         'Flask_JWT_Extended==4.4.4',
-        'Flask_WTF==1.0.1',
-        'SQLAlchemy==2.0.0b3',
-        'pymysql==1.0.2',
+        'Flask_WTF==1.1.1',
+        'SQLAlchemy==2.0.13',
+        'pymysql==1.0.3',
         'toml==0.10.2',
         'WTForms==3.0.1',
-        'email-validator==1.3.0',
-        'pika==1.3.1',
+        'email-validator==2.0.0',
+        'pika==1.3.2',
     ]
 )
